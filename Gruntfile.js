b3f7476e43aa276256d48b2ec063a7c70ddf58a8
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2015-05-22 11:53:21 -0700

    Copy to the same SVN directory we push

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,14 +46,14 @@ module.exports = function (grunt) {
                     mode :true
                 },
                 src: copy_files,
-                dest: 'releases/build/<%= pkg.name %>/trunk/'
+                dest: 'releases/<%= pkg.name %>/trunk/'
             },
             svn_tag: {
                 options : {
                     mode :true
                 },
                 src: copy_files,
-                dest: 'releases/build/<%= pkg.name %>/tags/<%= pkg.version %>/'
+                dest: 'releases/<%= pkg.name %>/tags/<%= pkg.version %>/'
             }
         },
         run: {
