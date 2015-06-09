8d9d75eccd7ba70d004c105acf4a84a2763394be
Jason Lemieux <jason@vernalcreative.com>
2015-06-09 00:59:42 -0400

    added some columns

--- includes/templates/about-panel.php
+++ includes/templates/about-panel.php
@@ -1,35 +1,40 @@
 <?php
 /**
- * About panel
- *
- * @package   Epoch
- * @author    Postmatic
- * @license   GPL-2.0+
- * @link
- * Copyright 2015 Transitive, Inc.
- */
+* About panel
+*
+* @package   Epoch
+* @author    Postmatic
+* @license   GPL-2.0+
+* @link
+* Copyright 2015 Transitive, Inc.
+*/
 ?>
 <h1>Welcome to Epoch</h1>
 <h2>Commenting Perfected - brought to you by Postmatic</h2>
 
-<h2>Epoch solves three big native commenting problems</h2>
+<p>Epoch solves three big native commenting problems</p>
+<div class="column">
 <h3>1. User Experience</h3>
 Comment forms and display are often neglected by theme developers. We've been there ourselves. The functions and templates involved in WordPress commenting are complicated, frustrating, and no fun to work with. Every day we see fantastic themes with ridiculously poor commenting experiences. So that's the first thing Epoch fixes.
 
 <strong>Epoch is now overrideing your native comment templates with a simple, elegant, and highly usable experience.</strong>
+</div>
 
+<div class="column">
 <h3>2. Speed and context</h3>
 No more submitting comments and watching the page reload. All Epoch comments are submitted via ajax and post instantly in their proper place within the conversation.
 
 And, as it should be, new comments from other users show up like magic automatically. It doesn't matter if they are posted from the web, or from email via <a href="http://gopostmatic.com">Postmatic</a>. The conversation is instantly updated.
+</div>
+
+<div class="column">
 <h3>3. Cacheing, page load, and CDNs</h3>
 Epoch is hands down the fastest commenting system available for WordPress, all the while supporting page cacheing and CDNs.Epoch comments lazy load into a placeholder container only when needed. This means your post will load instantly, your comments will load instantly, and your server won't blink. And no, we don't use iframes. Your full SEO mojo is in tact.
+</div>
 
 
 <h2>Success through compatibility</h2>
 Epoch is built for compatibility. Just like Postmatic. It uses fully native WordPress commenting, plays nicely with most all other commenting plugins, and employs WordPress coding standards. Use it with your favorite social login, voting, moderation, and other commenting plugins.
-<h2>Epoch is fully integrated with Postmatic</h2>
-When running Epoch along with <a href="http://gopostmatic.com">Postmatic</a> the magic really happens. The comment stream is updated in realtime with comments being posted from web as well as email. Subscribing to new comments is quick and simple while users are intelligently invited to become site subscribers as well. All without ugly checkboxes and obtrusive forms.
 
 <h3>Want to jump into the ring?</h3>
 This is just 1.0. We're hoping the WordPress community will lend strength, ideas, and code to this project. That's why we've kept it open-source. You can find Epoch on Github and jump right in. We can't wait to see where commenting goes next.
