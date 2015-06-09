7abdede7d1ae323609e95187fbcd2d1be60dd174
Jason Lemieux <jason@vernalcreative.com>
2015-06-09 12:50:38 -0400

    initial postmatic screen

--- includes/templates/postmatic-panel.php
+++ includes/templates/postmatic-panel.php
@@ -9,11 +9,17 @@
  * Copyright 2015 Transitive, Inc.
  */
 ?>
-<h1>Send new comment notifictions - <strong>and let them hit reply</strong>.</h1>
+<h1>Engage your readers at their own pace with 100% email-based commenting</h1>
+<h3>The days of one-way notifications are over. It’s time to let them hit reply.</h3>
+<p>Increased engagement is just the natural result when you make it easy for your readers to be a part of the conversation.</p>
 
-<p>Epoch is an amazing plugin that brings fantastic new technology to WordPress commenting - but it is only half the picture.</p>
+<p>Postmatic lets your readers subscribe by email when they leave a comment. Subsequent comments and replies will land in their inbox, just like with <em>Jetpack</em> or <em>Subscribe to Comments</em>. <strong>Except now with Postmatic, they can comment back and keep the conversation going just by hitting reply</strong>. They never have to leave their inbox.</p>
 
-<p>Add Postmatic to your site (it's free!) to enable two-way email comment notifications. Two way? Yes. The days of <em>notifications</em> are over. <strong>It's time to let them hit reply</strong>.</p>
+<p>Online or offline, everyone stays in the loop—regardless of web, desktop or mobile.</p>
+
+<a id="ttfmake-1426191218" class="ttfmake-button wistia-popover[height=450,playerColor=528693,width=800]" style="background-color: #000000; color: #ffffff; font-size: 17px; font-weight: bold; padding: 4px 10px; border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-right-radius: 3px; border-bottom-left-radius: 3px;" href="//fast.wistia.net/embed/iframe/e9ylk540qv?popover=tru" data-hover-background-color="#e5e5e5" data-hover-color="#000000"><i class="ttfmake-button-icon fa fa-play-circle-o"></i> Watch the Video</a>
+
+<script src="//fast.wistia.com/assets/external/popover-v1.js" charset="ISO-8859-1"></script>
 
 <h2>Epoch + Postmatic</h2>
 <p>When running Epoch along with Postmatic the magic really happens. The comment stream is updated in realtime with comments being posted from web as well as email. Subscribing to new comments is quick and simple while users are intelligently invited to become site subscribers as well. All without ugly checkboxes and obtrusive forms.</p>
