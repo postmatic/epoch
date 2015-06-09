abc964a4f4189b9228dc7970a2d3afe7aa887a43
Jason Lemieux <jason@vernalcreative.com>
2015-06-09 13:24:00 -0400

    plugs

--- includes/templates/about-panel.php
+++ includes/templates/about-panel.php
@@ -56,6 +56,6 @@
 </div>
 
 <h3>Contributors</h3>
-<p>Epoch is a collaboration between <a href="http://gopostmatic.com">Postmatic</a> and <a href="http://calderawp.com">CalderaWP</a>.</p>
+<p>Epoch is a collaboration between <a href="http://gopostmatic.com">Postmatic</a> and <a href="http://calderawp.com">CalderaWP</a>. We knew WordPress commenting could be better. Together we made it happen.</p>
 
 <p>This is just 1.0. We're hoping the WordPress community will lend strength, ideas, and code to this project. That's why we've kept it open-source. You can find Epoch on Github and jump right in. We can't wait to see where commenting goes next.</p>
