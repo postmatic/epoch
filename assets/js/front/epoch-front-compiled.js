6e60132a9180f3fb3068cd51b757ad6637d2a1ab
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2015-09-08 22:20:07 -0700

    Add compiled files for 1.0.4

--- assets/js/front/epoch-front-compiled.js
+++ assets/js/front/epoch-front-compiled.js
@@ -466,7 +466,10 @@ jQuery( document ).ready( function ( $ ) {
                         html = app.parse_comment( comment );
                         app.put_comment_in_dom( html, comment.comment_parent, comment.depth, parseInt( comment.comment_ID, 10 ) );
                     });
-
+                    
+                    if ( !is_new ) {
+                        jQuery( 'body' ).triggerHandler( 'epoch.comments.loaded' );   
+                    }
 
                 }
 
@@ -497,8 +500,6 @@ jQuery( document ).ready( function ( $ ) {
                     response = app.get_data_from_response( response );
 
                     app.comment_response( response, true );
-                    
-                    jQuery( 'body' ).triggerHandler( 'epoch.comments.loaded' );
 
 
                 }
