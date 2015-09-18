305afcfa611989755b12b36aa392351c2dfa739d
Ronald Huereca <ronalfy@gmail.com>
2015-09-17 20:00:24 -0500

    Adding moderation buttons to front-end.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- classes/front/api_helper.php
+++ classes/front/api_helper.php
@@ -142,6 +142,23 @@ class api_helper {
 		}else{
 			$comment[ 'reply_link' ] = '';
 		}
+		
+		$comment_moderation_output = array();
+        if ( ( current_user_can( 'manage_network' ) || current_user_can( 'manage_options' ) || current_user_can( 'moderate_comments' ) ) ) {
+            $comment[ 'front_moderation' ] = apply_filters( 'epoch_enable_frontend_moderation', true );
+            $comment_moderation_output[ 'approve_link' ] = sprintf( '<a href="#">%s</a>', esc_html__( 'Approve', 'epoch' ) );
+            $comment_moderation_output[ 'unapprove_link' ] = sprintf( '<a href="#">%s</a>', esc_html__( 'Unapprove', 'epoch' ) );
+            $comment_moderation_output[ 'trash_link' ] = sprintf( '<a href="#">%s</a>', esc_html__( 'Trash', 'epoch' ) );
+            $comment_moderation_output[ 'spam_link' ] = sprintf( '<a href="#">%s</a>', esc_html__( 'Spam', 'epoch' ) );
+            /* Comment Approved */
+            /* No output for spam/trashed comments because these should not be displayed on the front-end */
+                $comment[ 'approval_status' ] = (bool)$comment[ 'comment_approved' ];
+                $comment[ 'approve_link' ] = $comment_moderation_output[ 'approve_link' ];
+                $comment[ 'unapprove_link' ] = $comment_moderation_output[ 'unapprove_link' ];
+                $comment[ 'trash_link' ] = $comment_moderation_output[ 'trash_link' ];
+                $comment[ 'spam_link' ] = $comment_moderation_output[ 'spam_link' ];
+            
+        }
 
 
 		wp_cache_set( $key, $comment, self::$cache_group, HOUR_IN_SECONDS );
