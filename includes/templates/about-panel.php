c6303b2fd4443a93bc9fff713f8da35f9f9cb001
Jason Lemieux <jason@gopostmatic.com>
2015-09-29 02:13:03 +0000

    crowd control styles

--- includes/templates/about-panel.php
+++ includes/templates/about-panel.php
@@ -67,6 +67,6 @@
 </div>
 
 <h3 class="clear">Contributors</h3>
-<p>Epoch (we like to pronounce it 'epic') is a collaboration between <a href="http://gopostmatic.com">Postmatic</a> and <a href="http://calderawp.com">CalderaWP</a>. We knew WordPress commenting could be better. Together we made it happen. Need a hand with developing your own dream plugin? Check out <a href="https://calderawp.com/custom-plugin-development/">CalderaWP</a>.</p>
+<p>Epoch (we like to pronounce it 'epic') is a collaboration between <a href="http://gopostmatic.com">Postmatic</a> and <a href="http://calderawp.com">CalderaWP</a> with a growing list of <a href="https://github.com/postmatic/epoch/graphs/contributors">contributors</a>. We knew WordPress commenting could be better. Together we made it happen. Need a hand with developing your own dream plugin? Check out <a href="https://calderawp.com/custom-plugin-development/">CalderaWP</a>.</p>
 
 <p>This is just 1.0. We're hoping the WordPress community will lend strength, ideas, and code to this project. That's why we've kept it open-source. You can find <a href="http://github.com/postmatic/epoch" target="_blank">Epoch on Github</a> and jump right in. We can't wait to see where commenting goes next.</p>
